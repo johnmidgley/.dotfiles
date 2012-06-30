6dcc274bf68a704c70ed10fdd00ad73acc5a1c57
John Midgley <john.midgley@gmail.com>
2012-06-29 19:02:47 -0700

    Added emacs solarized, and linked to emacs init to .dotfiles

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,2 +1,5 @@
 (add-to-list 'load-path "~/.emacs.d/packages/")
+(add-to-list 'load-path "~/.dotfiles/solarized/emacs")
+(require 'color-theme-solarized)
 (require 'package) (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/")) (package-initialize)
+
