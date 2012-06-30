3832141c934b6927a32ab268ca9c6480b9f34ace
John Midgley <john.midgley@gmail.com>
2012-06-30 08:51:39 -0700

    Added "send expression to REPL" function

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,5 +6,59 @@
 (require 'color-theme-solarized)
 (eval-after-load "color-theme"
   '(progn
-     (color-theme-solarized-dark)))
+     (color-theme-solarized-light)))
+
+(defun slime-send-dwim (arg)
+  "Send the appropriate forms to CL to be evaluated."
+  (interactive "P")
+  (save-excursion
+    (cond
+      ;;Region selected - evaluate region
+      ((not (equal mark-active nil))
+       (copy-region-as-kill (mark) (point)))
+      ;; At/before sexp - evaluate next sexp
+      ((or (looking-at "(")
+           (save-excursion
+             (ignore-errors (forward-char 1))
+             (looking-at "(")))
+       (forward-list 1)
+       (let ((end (point))
+             (beg (save-excursion
+                    (backward-list 1)
+                    (point))))
+         (copy-region-as-kill beg end)))
+      ;; At/after sexp - evaluate last sexp
+      ((or (looking-at ")")
+           (save-excursion
+             (backward-char 1)
+             (looking-at ")")))
+       (if (looking-at ")")
+           (forward-char 1))
+       (let ((end (point))
+             (beg (save-excursion
+                    (backward-list 1)
+                    (point))))
+         (copy-region-as-kill beg end)))
+      ;; Default - evaluate enclosing top-level sexp
+      (t (progn
+           (while (ignore-errors (progn
+                                   (backward-up-list)
+                                   t)))
+           (forward-list 1)
+           (let ((end (point))
+                 (beg (save-excursion
+                        (backward-list 1)
+                        (point))))
+             (copy-region-as-kill beg end)))))
+    (set-buffer (slime-output-buffer))
+    (unless (eq (current-buffer) (window-buffer))
+      (pop-to-buffer (current-buffer) t))
+    (goto-char (point-max))
+    (yank)
+    (if arg (progn
+              (slime-repl-return)
+              (other-window 1)))))
+
+(add-hook 'slime-connected-hook (lambda ()
+  (define-key slime-mode-map (kbd "C-c s") 'slime-send-dwim)))
 
