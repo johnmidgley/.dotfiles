593b87137a54948d65fcc823e14c9bfbfbd2c51b
John Midgley <john.midgley@gmail.com>
2012-06-29 19:06:50 -0700

    init.el cleanup

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,5 +1,10 @@
 (add-to-list 'load-path "~/.emacs.d/packages/")
 (add-to-list 'load-path "~/.dotfiles/solarized/emacs")
-(require 'color-theme-solarized)
+
 (require 'package) (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/")) (package-initialize)
+
+(require 'color-theme-solarized)
+(eval-after-load "color-theme"
+  '(progn
+     (color-theme-solarized-dark)))
 
