062dfb181754c33f5696fdc2ebcea451d9a99bb6
John Midgley <john.midgley@gmail.com>
2013-01-23 13:13:15 -0800

    Added fish config

---
+++ config.fish
@@ -0,0 +1,4 @@
+set PATH $PATH ~/bin
+alias cdst "cd ~/dev/groovy-social-tools"
+alias aquamacs /Applications/Aquamacs.app/Contents/MacOS/Aquamacs
+alias joyspring "rlwrap java -jar ~/dev/joyspring/target/joyspring-0.1.0-SNAPSHOT-standalone.jar"
