291e88a87d4ecdb43e1474ebfaa93707acc1ea04
John Midgley <john.midgley@gmail.com>
2013-02-21 20:51:10 -0800

    Set Sublime as text editor

--- config.fish
+++ config.fish
@@ -1,4 +1,5 @@
 set PATH $PATH ~/bin
+setenv EDITOR "subl -w"
 alias cdst "cd ~/dev/groovy-social-tools"
 alias aquamacs /Applications/Aquamacs.app/Contents/MacOS/Aquamacs
 alias joyspring "rlwrap java -jar ~/dev/joyspring/target/joyspring-0.1.0-SNAPSHOT-standalone.jar"
