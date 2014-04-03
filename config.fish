0e5e9697933e6a5e70361381e63a68a55bc0a48c
John Midgley <john.midgley@gmail.com>
2014-04-02 21:41:28 -0700

    Moved backup from alias to bin script.

--- config.fish
+++ config.fish
@@ -4,4 +4,4 @@ setenv JAVA_HOME /System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Hom
 alias cdst "cd ~/dev/groovy-social-tools"
 alias aquamacs /Applications/Aquamacs.app/Contents/MacOS/Aquamacs
 alias joyspring "rlwrap java -jar ~/dev/joyspring/target/joyspring-0.1.0-SNAPSHOT-standalone.jar"
-alias backup "rsync -rlptP --delete-excluded --delete-before --exclude-from ~/.dotfiles/backup-excludes.txt ~ /Volumes/MiniMe/Backups"
+# alias backup "rsync -rlptP --delete-excluded --delete-before --exclude-from ~/.dotfiles/backup-excludes.txt ~ /Volumes/MiniMe/Backups"
