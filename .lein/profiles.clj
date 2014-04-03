8350d4e14072a7198bf9482a8ebc1c3f86c98bf7
John Midgley <john.midgley@gmail.com>
2014-04-02 21:39:02 -0700

    Added oneoff, exec and midje

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,1 +1,3 @@
-{:user {:plugins [[lein-oneoff "0.3.0"] [lein-exec "0.3.0"]]}}
+{:user {:plugins [[lein-oneoff "0.3.0"] 
+                  [lein-exec "0.3.0"]
+                  [lein-midje "3.0.0"]]}}
