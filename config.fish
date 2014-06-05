fc55f43611d3ef920ab7a9a4aba2ee51f62ddd49
John Midgley <john.midgley@gmail.com>
2014-06-04 22:10:37 -0700

    Added emacs alias

--- config.fish
+++ config.fish
@@ -3,5 +3,6 @@ setenv EDITOR "subl -w"
 setenv JAVA_HOME /System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
 alias cdst "cd ~/dev/groovy-social-tools"
 alias aquamacs /Applications/Aquamacs.app/Contents/MacOS/Aquamacs
+alias emacs /Applications/Emacs.app/Contents/MacOS/Emacs
 alias joyspring "rlwrap java -jar ~/dev/joyspring/target/joyspring-0.1.0-SNAPSHOT-standalone.jar"
 # alias backup "rsync -rlptP --delete-excluded --delete-before --exclude-from ~/.dotfiles/backup-excludes.txt ~ /Volumes/MiniMe/Backups"
