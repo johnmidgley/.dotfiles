9123703104fa0b568eb4cb6263b1db36131572a3
John Midgley <john.midgley@gmail.com>
2015-03-15 13:37:36 -0700

    Added ring to rein profiles

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[lein-oneoff "0.3.0"] 
                   [lein-exec "0.3.0"]
-                  [lein-midje "3.0.0"]]}}
+                  [lein-midje "3.0.0"]
+                  [lein-ring "0.9.1"]]}}
