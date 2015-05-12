93c92cd805db0d8b59e08d5c2d627d2d39050310
John Midgley <john.midgley@gmail.com>
2015-05-12 09:38:20 -0700

    Made emacs non-blocking at startup.

--- config.fish
+++ config.fish
@@ -1,8 +1,9 @@
 set PATH $PATH ~/bin
 setenv EDITOR "subl -w"
-setenv JAVA_HOME /System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
+setenv JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home 
 alias cdst "cd ~/dev/groovy-social-tools"
 alias aquamacs /Applications/Aquamacs.app/Contents/MacOS/Aquamacs
 alias emacs /Applications/Emacs.app/Contents/MacOS/Emacs
 alias joyspring "rlwrap java -jar ~/dev/joyspring/target/joyspring-0.1.0-SNAPSHOT-standalone.jar"
+alias emacs='open -a /Applications/Emacs.app $1'
 # alias backup "rsync -rlptP --delete-excluded --delete-before --exclude-from ~/.dotfiles/backup-excludes.txt ~ /Volumes/MiniMe/Backups"
