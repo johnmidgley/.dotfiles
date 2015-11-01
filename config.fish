a5dd889b663cf7ce72627d90e7035147e2ae06b0
John Midgley <john.midgley@gmail.com>
2015-10-31 23:42:14 -0700

    JDK Update

--- config.fish
+++ config.fish
@@ -1,7 +1,7 @@
 set PATH $PATH ~/bin ~/dev/clojurescript/bin .
 setenv EDITOR "subl -w"
-setenv JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.7.0_71.jdk/Contents/Home 
+setenv JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home
 setenv CLOJURESCRIPT_HOME ~/dev/clojurescript/
-alias joyspring "rlwrap java -jar ~/dev/joyspring/target/joyspring-0.1.0-SNAPSHOT-standalone.jar"
+alias joyspring "rlwrap java -jar ~/dev/netflix/joyspring/target/joyspring-0.1.0-SNAPSHOT-standalone.jar"
 alias emacs 'open -a /Applications/Emacs.app $1'
 # alias backup "rsync -rlptP --delete-excluded --delete-before --exclude-from ~/.dotfiles/backup-excludes.txt ~ /Volumes/MiniMe/Backups"
