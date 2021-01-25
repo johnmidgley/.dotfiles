3a7707ab11584082eaf39f0e91d0c63126fc8b09
John Midgley <john.midgley@gmail.com>
2021-01-24 16:39:04 -0800

    Updated Brew install

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 ## Brew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 ## Git
 git config --global user.name "John Midgley"
