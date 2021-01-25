7974ddd0fa41a07fcccb3e2ebe4c117aaa7dfb3d
John Midgley <john.midgley@gmail.com>
2021-01-24 17:44:33 -0800

    Fixed merge

--- config.fish
+++ config.fish
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 set PATH $PATH ~/bin ~/dev/clojurescript/bin /usr/local/bin /usr/local/sbin .
-=======
-set PATH ~/bin $PATH ~/dev/clojurescript/bin .
->>>>>>> 4b13d3d235e3b634f249f16a39718e7260d5a1e5
 setenv EDITOR "subl -w"
 setenv JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home
 setenv CLOJURESCRIPT_HOME ~/dev/clojurescript/
